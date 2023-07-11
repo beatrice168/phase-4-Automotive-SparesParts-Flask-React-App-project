ec706c38bdb829c48e3f9092179037efef3dc268
beatrice <helgapaul389@gmail.com>
2023-07-11 12:04:44 +0300

    made some changes

--- client/my-app/src/search.js
+++ client/my-app/src/search.js
@@ -1,17 +1,20 @@
-function Search({ searchTerm, onSearchChange }) {
-    return (
-      <div className="searchbar">
-        <label htmlFor="search">Search Cars:</label>
-        <input
-          type="text"
-          id="search"
-          placeholder="Type a name to search..."
-          value={searchTerm}
-          onChange={(e) => onSearchChange(e.target.value)}
-        />
-      </div>
-    );
-  }
-  
-  export default Search;
-  
+import React from 'react';
+
+const Search = ({ searchTerm, onSearchChange }) => {
+  const handleInputChange = (event) => {
+    onSearchChange(event.target.value);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default Search;
